2d840ab9e0376a211462d5030f247cf03aa786d3
rida_ad <adardour.rida@gmail.com>
2025-05-26 21:56:37 +0200

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+import asyncio
+from app.data_generator import (
+    bibliotheques, salles, visites, reservations, prets,
+    generate_bibliotheques, generate_salles, generate_etudiants,
+    generate_livres, generate_dynamic_data
+)
+
+app = FastAPI()
+
+@app.get("/visites_bu")
+def get_visites():
+    return visites
+
+@app.get("/reservations_salles")
+def get_reservations():
+    return reservations
+
+@app.get("/prets")
+def get_prets():
+    return prets
+
+@app.on_event("startup")
+async def startup():
+    generate_bibliotheques()
+    generate_salles()
+    generate_etudiants()
+    generate_livres()
+    asyncio.create_task(generate_dynamic_data())
